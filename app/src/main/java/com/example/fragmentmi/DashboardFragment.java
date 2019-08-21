71a2d0981b7da44181c97e3d926b5b55f725f6e7
Muhammad Fariz Fakhruddin <mfakhruddin2237@gmail.com>
2019-08-21 10:11:50 +0700

    first commit

---
+++ app/src/main/java/com/example/fragmentmi/DashboardFragment.java
@@ -0,0 +1,18 @@
+package com.example.fragmentmi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle sis){
+
+        return inflater.inflate(R.layout.fragment_dashboard, null);
+
+    }
+}
---
+++ app/src/main/java/com/example/fragmentmi/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.fragmentmi;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView navView = findViewById(R.id.navigation);
+        navView.setOnNavigationItemSelectedListener(this);
+    }
+
+    Fragment fragment = null;
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    fragment = new DashboardFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    fragment = new NotificationFragment();
+                    break;
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+            }
+            return loadFragment(fragment);
+    }
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
